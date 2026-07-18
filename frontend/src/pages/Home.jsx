93c9a18292db135609ad67ecb447bff7aad02332
kritikabharti <kritikabharti577@gmail.com>
2026-07-18 17:25:13 +0530

    images change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,6 +46,10 @@ import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
 import img4 from "../assets/img4.jpg";
+import img5 from "../assets/img5.jpg";
+import img6 from "../assets/img6.jpeg";
+import img7 from "../assets/img7.jpg";
+import img8 from "../assets/img8.jpg";
 
 import aboutImage from '../assets/prooo.jpg';
 import teamImage from '../assets/unnn.jpg';
@@ -153,6 +157,8 @@ const scrollToCourses = () => {
     });
   };
 
+
+  
 
 
   return (
@@ -816,7 +822,7 @@ className="courses-section">
           ease: "linear",
         }}
       >
-        {[img1, img2, img3, img4, img1, img2, img3, img4].map(
+        {[img1, img2, img3, img4, img5, img6, img7, img8].map(
           (img, index) => (
             <div className="image-card" key={index}>
               <img src={img} alt="" className="gallery-image" />
