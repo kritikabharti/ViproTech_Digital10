bb246bb26a19279a017b4ce18df26eff19907867
kritikabharti <kritikabharti577@gmail.com>
2026-07-20 15:24:07 +0530

    teams add

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import cloudinary from '../config/cloudinary.js';
 
+
 // Configure Cloudinary storage
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
